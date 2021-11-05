559299634fa29e0cb68f53e7c90499c5e06e3bca
AmiraBelkis <ia_feliachi@esi.dz>
2021-11-05 18:38:19 +0100

    adding 2nd scean

---
+++ script.js
@@ -0,0 +1,10 @@
+function Start() {
+    let main = document.getElementById('main'),
+        start = document.getElementById('start'),
+        title = document.getElementById('title'),
+        subtitle = document.getElementById('subTitle');
+    main.style.display = 'flex';
+    start.style.left = '120%';
+    subtitle.style.left = '120%';
+    title.style.left = '33%';
+}
